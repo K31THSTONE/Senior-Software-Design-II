9a691a125122e5ff2de1b46837c34f284c55c947
amutzaba <amutzaba@ycp.edu>
2018-02-18 15:24:16 -0500

    Synchronized movement
    Two seperate entities are able to see one another move in real time properly now.
    
    Look in Asset folder files for code written by Greg and Alex in regards to this commit.
    Alex set up the PlayerSetup which allows the player to properly disable or enable the correct components so the game knows which parts it needs the network to transmit. Greg was able to set up the two needed components including the NetworkTransform and NetworkTransformChild.

--- Assets/PlayerSetup.cs
+++ Assets/PlayerSetup.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerSetup : MonoBehaviour {
+public class PlayerSetup : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    Behaviour[] disabledComponents;
+
+    Camera sceneCamera;
+
+    //disabling components to allow proper networking between players and objects
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < disabledComponents.Length; i++)
+            {
+                disabledComponents[i].enabled = false;
+            }
+        } else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+
 }
