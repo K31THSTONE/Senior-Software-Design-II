b2af446f4ed737ae009b2b2a8653a7136abef9d1
amutzaba <amutzaba@ycp.edu>
2018-02-19 12:35:02 -0500

    Small mechanic adjustments
    Allow the player to jump with a slight fly/hover type of action that brings them back down after releasing space, also added floor collision

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSens = 3f;
+    [SerializeField]
+    private float flyForce = 750f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+
+    }
+
+    private void Update()
+    {
+        //3d movement vector
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        Vector3 _velocity = (movHorizontal + movVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        //rotation calc to turn around
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, yRot, 0f) * lookSens;
+
+        motor.Rotate(_rotation);
+
+
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _camRotation = new Vector3(xRot, 0f, 0f) * lookSens;
+
+        motor.RotateCamera(_camRotation);
+
+        Vector3 _flyForce = Vector3.zero;
+
+        //jump force
+        if (Input.GetButton("Jump"))
+        {
+            _flyForce = Vector3.up * flyForce;
+        }
+
+        motor.UseFly(_flyForce);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMotor : MonoBehaviour {
+
+    [SerializeField]
+    private Camera cam;
+
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 camRotation = Vector3.zero;
+    private Vector3 flyForce = Vector3.zero;
+
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    public void Move (Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _camRotation)
+    {
+        camRotation = _camRotation;
+    }
+    //gets vector for flying
+    public void UseFly (Vector3 _flyForce)
+    {
+        flyForce = _flyForce;
+    }
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+        
+    }
+
+    void PerformMovement ()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        if (flyForce != Vector3.zero)
+        {
+            rb.AddForce(flyForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-camRotation);
+        }
+    }
+
+
+}
