4c4e96de5633f16d0463019ae64056a5d85d1122
amutzaba <amutzaba@ycp.edu>
2018-03-26 12:05:28 -0400

    Beginning of match hosting/server browsing
    Added a room creation function within the HostGame script which starts up the hosting process for a player

---
+++ Assets/Scripts/HostGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+
+    //can change dependent on what size room a person wants to find
+    [SerializeField]
+    private uint roomSize = 2;
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    //allows room name creation
+    public void SetName (string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoom ()
+    {
+        if (roomName != "" && roomName != null)
+        {
+            Debug.Log("Making room:" + roomName + "with" + roomSize + "players allowed");
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
