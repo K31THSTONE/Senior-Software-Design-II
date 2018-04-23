7db989c6e2536d4277dc8f18f0ee79208f6ab5db
K31THSTONE <goliff@ycp.edu>
2018-04-23 02:04:35 +0100

    Took away needless redundency while keeping inheritance in playerWeapons script

--- Assets/Scripts/PlayerWeapons.cs
+++ Assets/Scripts/PlayerWeapons.cs
@@ -92,68 +92,66 @@ public class PlayerWeapons  {
     {
         this.graphics = graphics;
     }
-}
-
+	public static void initializePistol(){
+		this.setName("Pistol");
+		this.setDmg(10);
+		this.setRng(75f);
+		this.setFireRate(0f);
+		this.setBullets(80);
+		this.setReloadTime(0f);
+		//this.setGraphics();
+		this.setClipSize(8);
+		this.setMaxAmmo(80);
+	}
+	public static void initializeSubmachine(){
+		this.setName("Sub Machine Gun");
+		this.setDmg(5);
+		this.setRng(55f);
+		this.setFireRate(2f);
+		this.setBullets(150);
+		this.setReloadTime(2f);
+		//this.setGraphics();
+		this.setClipSize(30);
+		this.setMaxAmmo(150);
+	}
+	public static void initializeSniper(){
+		this.setName("Sniper");
+		this.setDmg(100);
+		this.setRng(750f);
+		this.setFireRate(0f);
+		this.setBullets(10);
+		this.setReloadTime(5f);
+		//this.setGraphics();
+		this.setClipSize(1);
+		this.setMaxAmmo(10);
+	}
+	public static void initializeShotgun(){
+		this.setName("Sawed Off Shotgun");
+		this.setDmg(80);
+		this.setRng(35f);
+		this.setFireRate(0f);
+		this.setBullets(30);
+		this.setReloadTime(5f);
+		//this.setGraphics();
+		this.setClipSize(2);
+		this.setMaxAmmo(30);
+	}
 public class Pistol : PlayerWeapons
 {
-    public static void initializePistol(PlayerWeapons playerWeapon)
-    {
-        playerWeapon.setName("Pistol");
-        playerWeapon.setDmg(10);
-        playerWeapon.setRng(75f);
-        playerWeapon.setFireRate(0f);
-        playerWeapon.setBullets(80);
-        playerWeapon.setReloadTime(0f);
-        //playerWeapon.setGraphics()
-        playerWeapon.setClipSize(8);
-        playerWeapon.setMaxAmmo(80);
-    }
+  
 }
 
 public class Submachine : PlayerWeapons
 {
-    public void initializeSubmachine(PlayerWeapons playerWeapon)
-    {
-        playerWeapon.setName("Sub Machine Gun");
-        playerWeapon.setDmg(5);
-        playerWeapon.setRng(55f);
-        playerWeapon.setFireRate(2f);
-        playerWeapon.setBullets(150);
-        playerWeapon.setReloadTime(2f);
-        //playerWeapon.setGraphics()
-        playerWeapon.setClipSize(30);
-        playerWeapon.setMaxAmmo(150);
-    }
+    
 }
 
 public class Sniper : PlayerWeapons
 {
-    public void initializeSniper(PlayerWeapons playerWeapon)
-    {
-        playerWeapon.setName("Sniper");
-        playerWeapon.setDmg(100);
-        playerWeapon.setRng(750f);
-        playerWeapon.setFireRate(0f);
-        playerWeapon.setBullets(10);
-        playerWeapon.setReloadTime(5f);
-        //playerWeapon.setGraphics()
-        playerWeapon.setClipSize(1);
-        playerWeapon.setMaxAmmo(10);
-    }
+    
 }
 
 public class Shotgun : PlayerWeapons
 {
-    public void initializeShotgun(PlayerWeapons playerWeapon)
-    {
-        playerWeapon.setName("Sawed Off Shotgun");
-        playerWeapon.setDmg(80);
-        playerWeapon.setRng(35f);
-        playerWeapon.setFireRate(0f);
-        playerWeapon.setBullets(30);
-        playerWeapon.setReloadTime(5f);
-        //playerWeapon.setGraphics()
-        playerWeapon.setClipSize(2);
-        playerWeapon.setMaxAmmo(30);
-    }
+    
 }
