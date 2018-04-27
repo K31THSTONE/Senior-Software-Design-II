5d77c28d6753a86d00b8a0585a8eac313cb9ed51
K31THSTONE <goliff@ycp.edu>
2018-04-27 06:37:22 +0100

    Consolidated script content involving players into playerController, kept files for now

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,57 @@ using UnityEngine;
 [RequireComponent(typeof(ConfigurableJoint))]
 
 public class PlayerController : MonoBehaviour {
+	
+	//PlayerShootFields
+	 private const string PLAYER_TAG = "Player";
+    [SerializeField]
+    private PlayerWeapons weapon = new PlayerWeapons();
 
+    [SerializeField]
+    private GameObject weaponGraphics;
+
+    [SerializeField]
+    private string weaponLayername = "Weapon";
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+	//
+	
+	//PlayerSetupFields
+	[SerializeField]
+    Behaviour[] disabledComponents;
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+    [SerializeField]
+    string dontDrawLayer = "DontDraw";
+    [SerializeField]
+    GameObject playerGraphics;
+    [SerializeField]
+    GameObject playerUI;
+    private GameObject playerUIInstance;
+
+    Camera sceneCamera;
+	//
+	
+	//PlayerMotorFields
+	[SerializeField]
+    private Camera cam;
+
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private float camRotationX = 0f;
+    private float currentCamRotationX = 0f;
+    private Vector3 flyForce = Vector3.zero;
+
+    [SerializeField]
+    private float cameraRotationLim = 85f;
+	
+	 private Rigidbody rb;
+	//
     [SerializeField]
     private float speed = 5f;
     [SerializeField]
@@ -23,7 +73,189 @@ public class PlayerController : MonoBehaviour {
 
     private PlayerMotor motor;
     private ConfigurableJoint joint;
+	//PlayerShootMethods
+	void Start ()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No cam referenced");
+            this.enabled = false;
+        }
+        Pistol.initializePistol(weapon);
+        weaponGraphics.layer = LayerMask.NameToLayer(weaponLayername);
+    }
 
+    void Update()
+    {
+        if (PauseMenu.IsOn)
+        {
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    [Client]
+    void Shoot ()
+    {
+        RaycastHit _hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.getRng(), mask) )
+        {
+            if (_hit.collider.tag == PLAYER_TAG)
+            {
+                CmdPlayerShot(_hit.collider.name, weapon.getDmg());
+            }
+        }
+    }
+
+    [Command]
+    void CmdPlayerShot (string _playerID, int _damage)
+    {
+        Debug.Log(_playerID + "has been shot");
+
+        Player _player = GameManager.GetPlayer(_playerID);
+        _player.RpcTakeDamage(_damage);
+    }
+	//PlayerSetupMethods
+	void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            DisableComponents();
+            AssignRemoteLayer();
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+
+            //get rid of local player gui
+            SetRecursiveLayer(playerGraphics, LayerMask.NameToLayer(dontDrawLayer));
+            //create playerUI
+            playerUIInstance = Instantiate(playerUI);
+            playerUIInstance.name = playerUI.name;
+            GetComponent<Player>().Setup();
+        }
+
+
+
+    }
+
+    void SetRecursiveLayer(GameObject obj, int newLayer)
+    {
+        obj.layer = newLayer;
+        foreach (Transform child in obj.transform)
+        {
+            //goes through each child layer down heirarchy
+            SetRecursiveLayer(child.gameObject, newLayer);
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        string _netID = GetComponent<NetworkIdentity>().netId.ToString();
+        Player _player = GetComponent<Player>();
+        GameManager.RegisterPlayer(_netID, _player);
+    }
+
+
+    void AssignRemoteLayer ()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void DisableComponents ()
+    {
+        for (int i = 0; i < disabledComponents.Length; i++)
+        {
+            disabledComponents[i].enabled = false;
+        }
+    }
+
+
+    void OnDisable()
+    {
+
+        Destroy(playerUIInstance);
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+
+        GameManager.UnregisterPlayer(transform.name);
+    }
+	//PlayerMotorMethods
+	void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    public void Move (Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(float _camRotationX)
+    {
+        camRotationX = _camRotationX;
+    }
+    //gets vector for flying
+    public void UseFly (Vector3 _flyForce)
+    {
+        flyForce = _flyForce;
+    }
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+        
+    }
+
+    void PerformMovement ()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        if (flyForce != Vector3.zero)
+        {
+            rb.AddForce(flyForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            //Set rotation and clamp so cannot rotate upside down
+            currentCamRotationX -= camRotationX;
+            currentCamRotationX = Mathf.Clamp(currentCamRotationX, -cameraRotationLim, cameraRotationLim);
+
+            //apply
+            cam.transform.localEulerAngles = new Vector3(currentCamRotationX, 0f, 0f);
+        }
+    }
+
+
+}
+	//PlayerControllerMethods
     private void Start()
     {
         motor = GetComponent<PlayerMotor>();
